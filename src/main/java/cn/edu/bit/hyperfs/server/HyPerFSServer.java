e2f4ad3450c3cce8e2cbad2b6eafb6bdcdd84581
Joyce <1071060893@qq.com>
2025-12-10 19:23:34 +0800

    用匿名内部类代替`HttpServerInitializer`

--- src/main/java/cn/edu/bit/hyperfs/server/HyPerFSServer.java
+++ src/main/java/cn/edu/bit/hyperfs/server/HyPerFSServer.java
@@ -2,9 +2,15 @@ package cn.edu.bit.hyperfs.server;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.MultiThreadIoEventLoopGroup;
 import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class HyPerFSServer {
     private final int port;
@@ -14,15 +20,23 @@ public class HyPerFSServer {
     }
 
     public void start() throws Exception {
-        // 创建 Reactor 线程组
-        var bossGroup = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory()); // 接收连接
-        var workerGroup = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory()); // 处理IO
+        // 创建Reactor线程组
+        var bossGroup = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory()); // 用于接收连接
+        var workerGroup = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory()); // 用于处理IO
 
         try {
             var serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new HttpServerInitializer()); // 下一步编写这个初始化器
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            ChannelPipeline channelPipeline = socketChannel.pipeline();
+                            channelPipeline.addLast(new HttpServerCodec()); // HTTP 编解码器
+                            channelPipeline.addLast(new HttpObjectAggregator(6553600)); // 限制最大消息大小
+                            channelPipeline.addLast(new ChunkedWriteHandler()); // 支持异步发送大的码流
+                            channelPipeline.addLast(new HttpServerHandler()); // 业务逻辑处理
+                        }
+                    });
 
             System.out.println("HyPerFS Server started on port " + port);
 
