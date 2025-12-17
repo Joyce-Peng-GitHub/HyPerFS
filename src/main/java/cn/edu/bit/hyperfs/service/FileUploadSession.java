f72359357c8ccbfab258ac04b68735d6270d2155
Joyce <1071060893@qq.com>
2025-12-18 03:16:20 +0800

    增加文件存储数据的数据库操作

--- src/main/java/cn/edu/bit/hyperfs/service/FileUploadSession.java
+++ src/main/java/cn/edu/bit/hyperfs/service/FileUploadSession.java
@@ -5,19 +5,20 @@ import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 
+import cn.edu.bit.hyperfs.db.FileStorageDao;
 import cn.edu.bit.hyperfs.entity.FileStorageData;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 
 public class FileUploadSession {
-	private final File tmpFile; // 临时文件句柄
+	private final File temporaryFile; // 临时文件句柄
 	private final FileChannel fileChannel; // NIO 文件通道 (高性能写入)
 	private final MessageDigest digest; // 哈希计算
 	private long receivedBytes = 0;
 
 	public FileUploadSession(File tmpDirectory) throws Exception {
-		this.tmpFile = File.createTempFile("upload_", ".tmp", tmpDirectory);
-		this.fileChannel = FileChannel.open(tmpFile.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+		this.temporaryFile = File.createTempFile("upload_", ".tmp", tmpDirectory);
+		this.fileChannel = FileChannel.open(temporaryFile.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
 		this.digest = MessageDigest.getInstance("SHA-256");
 	}
 
@@ -33,12 +34,6 @@ public class FileUploadSession {
 		// 更新哈希值
 		chunk.markReaderIndex();
 		digest.update(chunk.nioBuffer());
-		chunk.resetReaderIndex();
-		
-		chunk.markReaderIndex();
-		var buffer = new byte[length];
-		chunk.readBytes(buffer);
-		System.out.println(new String(buffer));
 		chunk.resetReaderIndex();
 
 		// 写入临时文件
@@ -56,17 +51,24 @@ public class FileUploadSession {
 		fileChannel.force(true);
 		fileChannel.close();
 
-		byte[] hashValue = digest.digest();
+		var hashValue = ByteBufUtil.hexDump(digest.digest());
 
-		// TODO: 检查文件是否已存在，若存在则删除临时文件并增加引用计数
-
-		// 将临时文件移动到数据目录
-		var finalFile = new File(dataDirectory, ByteBufUtil.hexDump(hashValue));
-		if (!tmpFile.renameTo(finalFile)) {
-			throw new Exception("Failed to move temporary file to data directory");
+		// 检查文件是否已存在，若存在则删除临时文件并增加引用计数
+		var fileStorageDao = new FileStorageDao();
+		var referenceCount = fileStorageDao.insertOrIncrement(hashValue, receivedBytes);
+		if (referenceCount > 1) {
+			// 文件已存在，删除临时文件
+			if (temporaryFile.exists()) {
+				temporaryFile.delete();
+			}
+		} else {
+			// 将临时文件移动到数据目录
+			var finalFile = new File(dataDirectory, hashValue);
+			if (!temporaryFile.renameTo(finalFile)) {
+				throw new Exception("Failed to move temporary file to data directory");
+			}
 		}
-
-		return new FileStorageData(hashValue, receivedBytes, 1);
+		return new FileStorageData(hashValue, receivedBytes, referenceCount);
 	}
 
 	/**
@@ -76,8 +78,8 @@ public class FileUploadSession {
 	 */
 	public void abort() throws Exception {
 		fileChannel.close();
-		if (tmpFile.exists()) {
-			tmpFile.delete();
+		if (temporaryFile.exists()) {
+			temporaryFile.delete();
 		}
 	}
 }
